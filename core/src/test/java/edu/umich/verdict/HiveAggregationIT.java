bd2daf45fc244c1e9497644157238f64ca1ce4c6
Yongjoo Park <pyongjoo@umich.edu>
2017-06-26 22:45:38 -0400

    synced with upstream

--- core/src/test/java/edu/umich/verdict/HiveAggregationIT.java
+++ core/src/test/java/edu/umich/verdict/HiveAggregationIT.java
@@ -31,5 +31,35 @@ public class HiveAggregationIT extends AggregationIT {
 		Connection conn = DriverManager.getConnection(url);
 		stmt = conn.createStatement();
 	}
+	
+	@Override
+	public void simpleCountDistinctUsingUniverseSample() throws VerdictException, SQLException {
+		super.simpleCountDistinctUsingUniverseSample();
+	}
+
+	@Override
+	public void simpleCountDistinctUsingUniverseSample2() throws VerdictException, SQLException {
+		super.simpleCountDistinctUsingUniverseSample2();
+	}
+
+	@Override
+	public void simpleCountDistinctUsingStratifiedSample() throws VerdictException, SQLException {
+		super.simpleCountDistinctUsingStratifiedSample();
+	}
+
+	@Override
+	public void simpleCountDistinctUsingStratifiedSample2() throws VerdictException, SQLException {
+		super.simpleCountDistinctUsingStratifiedSample2();
+	}
+
+	@Override
+	public void groupbyCountDistinctUsingUniverseSample() throws VerdictException, SQLException {
+		super.groupbyCountDistinctUsingUniverseSample();
+	}
+
+	@Override
+	public void groupbyCountDistinctUsingUniverseSample2() throws VerdictException, SQLException {
+		super.groupbyCountDistinctUsingUniverseSample2();
+	}
 
 }
