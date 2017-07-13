0cfa80c8c85790bcce79040e6f9e5d0ba820e775
Yongjoo Park <pyongjoo@umich.edu>
2017-07-13 13:12:17 -0400

    removed test suites

--- core-scala/src/main/scala/edu/umich/verdict/SparkIntegrationSuite.scala
+++ core-scala/src/main/scala/edu/umich/verdict/SparkIntegrationSuite.scala
@@ -3,19 +3,23 @@ package edu.umich.verdict
 import edu.umich.verdict.VerdictSparkContext
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.sql.DataFrame
-import org.scalatest.FunSuite
-import com.holdenkarau.spark.testing.SharedSparkContext
-import org.scalactic.source.Position.apply
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
 
 
-class SparkIntegrationSuite extends FunSuite with SharedSparkContext {
-
-	test("simple count") ({
-		val sqlContext = new HiveContext(sc)
+class SparkIntegrationSuite {
+    
+    def main(args: Array[String]) {
+        
+        val conf = new SparkConf()
+        conf.setMaster("spark://salat1.eecs.umich.edu:7077")
+        conf.setAppName("Spark Integration Tests")
+        val sc = new SparkContext(conf);
+        val sqlContext = new HiveContext(sc)
 		val vc = new VerdictSparkContext(sqlContext)
 		val sql: String = "select count(*) from instacart1g.orders"
 		val df: DataFrame = vc.sql(sql)
 		df.show(false)
-	})
-
+    }
+    
 }
