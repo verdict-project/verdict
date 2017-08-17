b5887d9ea2f708dea56ce808588506dbaf822f03
Yongjoo Park <pyongjoo@umich.edu>
2017-08-17 15:31:54 -0400

    temporary fixes for autocommit issue. link 1: https://github.com/mozafari/verdict/issues/83 link 2: http://salat3.eecs.umich.edu/bugzilla/show_bug.cgi?id=20

--- jdbc/src/main/java/edu/umich/verdict/jdbc/VerdictConnection.java
+++ jdbc/src/main/java/edu/umich/verdict/jdbc/VerdictConnection.java
@@ -33,19 +33,19 @@ import edu.umich.verdict.util.VerdictLogger;
 
 
 public class VerdictConnection implements Connection {
-//    private final DbConnector connector;
-//    private final Connection innerConnection;
+    //    private final DbConnector connector;
+    //    private final Connection innerConnection;
     private final VerdictConf conf;
     private VerdictJDBCContext vc;
     private boolean isOpen = true;
-    
+
     private Connection getDbmsConnection() {
-    	return ((DbmsJDBC) vc.getDbms()).getDbmsConnection();
+        return ((DbmsJDBC) vc.getDbms()).getDbmsConnection();
     }
-    
-//    public void setLogLevel(String level) {
-//    	vc.setLogLevel(level);
-//    }
+
+    //    public void setLogLevel(String level) {
+    //    	vc.setLogLevel(level);
+    //    }
 
     /**
      * Option priorities
@@ -57,75 +57,75 @@ public class VerdictConnection implements Connection {
      * @throws SQLException
      */
     public VerdictConnection(String url, Properties info) throws SQLException {
-    	try{
-	    	// set properties from the config file
-	    	if (info.contains("configfile")) {
-	    		conf = new VerdictConf(info.getProperty("configfile"));
-	    	} else {
-	    		conf = new VerdictConf();		// by default, this loads configs from the file
-	    	}
-	    	
-	    	// set properties from the passed connection string.
-	    	Pattern inlineOptions = Pattern.compile("(?<key>[a-zA-Z0-9_\\.]+)=[\"']?(?<value>[a-zA-Z0-9@_/\\.\\\\:-]+)[\"']?");
-	    	Matcher inlineMatcher = inlineOptions.matcher(url);
-	    	while (inlineMatcher.find()) {
-	    		info.setProperty(inlineMatcher.group("key"), inlineMatcher.group("value"));
-	    	}
-	    	conf.setProperties(info);
-	    	
-	    	for (Map.Entry<Object, Object> e : info.entrySet()) {
-	    		VerdictLogger.debug(this, String.format("connection properties: %s = %s", e.getKey(), e.getValue()));
-	    	}
-	    	
-	    	// set properties from the url string.    	
-	    	Pattern urlOptions = Pattern.compile("^jdbc:verdict:(?<dbms>\\w+)://(?<host>[\\.a-zA-Z0-9\\-]+)(?::(?<port>\\d+))?(?:/(?<schema>\\w+))?"
-	    										+ "(\\?(?<extras>.*))?");
+        try{
+            // set properties from the config file
+            if (info.contains("configfile")) {
+                conf = new VerdictConf(info.getProperty("configfile"));
+            } else {
+                conf = new VerdictConf();		// by default, this loads configs from the file
+            }
+
+            // set properties from the passed connection string.
+            Pattern inlineOptions = Pattern.compile("(?<key>[a-zA-Z0-9_\\.]+)=[\"']?(?<value>[a-zA-Z0-9@_/\\.\\\\:-]+)[\"']?");
+            Matcher inlineMatcher = inlineOptions.matcher(url);
+            while (inlineMatcher.find()) {
+                info.setProperty(inlineMatcher.group("key"), inlineMatcher.group("value"));
+            }
+            conf.setProperties(info);
+
+            for (Map.Entry<Object, Object> e : info.entrySet()) {
+                VerdictLogger.debug(this, String.format("connection properties: %s = %s", e.getKey(), e.getValue()));
+            }
+
+            // set properties from the url string.    	
+            Pattern urlOptions = Pattern.compile("^jdbc:verdict:(?<dbms>\\w+)://(?<host>[\\.a-zA-Z0-9\\-]+)(?::(?<port>\\d+))?(?:/(?<schema>\\w+))?"
+                    + "(\\?(?<extras>.*))?");
             Matcher urlMatcher = urlOptions.matcher(url);
             if (!urlMatcher.find())
                 throw new SQLException("Invalid URL.");
-            
+
             conf.setDbms(urlMatcher.group("dbms"));
             conf.setHost(urlMatcher.group("host"));
             if (urlMatcher.group("port") != null) {
-            	conf.setPort(urlMatcher.group("port"));
+                conf.setPort(urlMatcher.group("port"));
             } else {
-            	conf.setPort(conf.getDefaultPort());		// assume config file includes it.
+                conf.setPort(conf.getDefaultPort());		// assume config file includes it.
             }
             if (urlMatcher.group("schema") != null) {
-            	conf.setDbmsSchema(urlMatcher.group("schema"));
+                conf.setDbmsSchema(urlMatcher.group("schema"));
             }
-            
+
             String extras = urlMatcher.group("extras");
             if (extras != null) {
-	            Matcher extraMatcher = inlineOptions.matcher(extras);
-	            
-	            while (extraMatcher.find()) {
-	            	conf.set(extraMatcher.group("key"), extraMatcher.group("value"));
-	            }
+                Matcher extraMatcher = inlineOptions.matcher(extras);
+
+                while (extraMatcher.find()) {
+                    conf.set(extraMatcher.group("key"), extraMatcher.group("value"));
+                }
             }
-            
-    		this.vc = VerdictJDBCContext.from(conf); 
-            
+
+            this.vc = VerdictJDBCContext.from(conf); 
+
         } catch (VerdictException e) {
-			throw new SQLException(StackTraceReader.stackTrace2String(e));
-		}
+            throw new SQLException(StackTraceReader.stackTrace2String(e));
+        }
     }
 
 
     @Override
     public Statement createStatement() throws SQLException {
-    	// we create a copy of VerdictContext so that the underlying statement is not shared.
+        // we create a copy of VerdictContext so that the underlying statement is not shared.
         return new VerdictStatement(this, vc);
     }
 
     @Override
     public PreparedStatement prepareStatement(String sql) throws SQLFeatureNotSupportedException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support prepared statements");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support prepared statements");
     }
 
     @Override
     public CallableStatement prepareCall(String sql) throws SQLFeatureNotSupportedException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support callable statements");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support callable statements");
     }
 
     @Override
@@ -135,31 +135,31 @@ public class VerdictConnection implements Connection {
 
     @Override
     public void setAutoCommit(boolean autoCommit) throws SQLException {
-    	getDbmsConnection().setAutoCommit(autoCommit);
+        getDbmsConnection().setAutoCommit(autoCommit);
     }
 
     @Override
     public boolean getAutoCommit() throws SQLException {
-    	return getDbmsConnection().getAutoCommit(); 
+        return getDbmsConnection().getAutoCommit(); 
     }
 
     @Override
     public void commit() throws SQLException {
-    	getDbmsConnection().commit();
+        getDbmsConnection().commit();
     }
 
     @Override
     public void rollback() throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support updates (thus nor commits)");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support updates (thus nor commits)");
     }
 
     @Override
     public void close() throws SQLException {
         try {
-			vc.destroy();
-		} catch (VerdictException e) {
-			throw new SQLException(StackTraceReader.stackTrace2String(e));
-		}
+            vc.destroy();
+        } catch (VerdictException e) {
+            throw new SQLException(StackTraceReader.stackTrace2String(e));
+        }
         isOpen = false;
     }
 
@@ -170,37 +170,37 @@ public class VerdictConnection implements Connection {
 
     @Override
     public DatabaseMetaData getMetaData() throws SQLException {
-    	return getDbmsConnection().getMetaData();
+        return getDbmsConnection().getMetaData();
     }
 
     @Override
     public void setReadOnly(boolean readOnly) throws SQLException {
-    	getDbmsConnection().setReadOnly(readOnly);
+        getDbmsConnection().setReadOnly(readOnly);
     }
 
     @Override
     public boolean isReadOnly() throws SQLException {
-    	return getDbmsConnection().isReadOnly();
+        return getDbmsConnection().isReadOnly();
     }
 
     @Override
     public void setCatalog(String catalog) throws SQLException {
-    	getDbmsConnection().setCatalog(catalog);
+        getDbmsConnection().setCatalog(catalog);
     }
 
     @Override
     public String getCatalog() throws SQLException {
-    	return getDbmsConnection().getCatalog();
+        return getDbmsConnection().getCatalog();
     }
 
     @Override
     public void setTransactionIsolation(int level) throws SQLException {
-    	getDbmsConnection().setTransactionIsolation(level);
+        getDbmsConnection().setTransactionIsolation(level);
     }
 
     @Override
     public int getTransactionIsolation() throws SQLException {
-    	return getDbmsConnection().getTransactionIsolation();
+        return getDbmsConnection().getTransactionIsolation();
     }
 
     @Override
@@ -210,22 +210,22 @@ public class VerdictConnection implements Connection {
 
     @Override
     public void clearWarnings() throws SQLException {
-    	getDbmsConnection().clearWarnings();
+        getDbmsConnection().clearWarnings();
     }
 
     @Override
     public Statement createStatement(int resultSetType, int resultSetConcurrency) throws SQLException {
-    	return new VerdictStatement(this, vc);
+        return new VerdictStatement(this, vc);
     }
 
     @Override
     public PreparedStatement prepareStatement(String sql, int resultSetType, int resultSetConcurrency) throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support prepared statements");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support prepared statements");
     }
 
     @Override
     public CallableStatement prepareCall(String sql, int resultSetType, int resultSetConcurrency) throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support callable statements");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support callable statements");
     }
 
     @Override
@@ -235,12 +235,12 @@ public class VerdictConnection implements Connection {
 
     @Override
     public void setTypeMap(Map<String, Class<?>> map) throws SQLException {
-    	getDbmsConnection().setTypeMap(map);
+        getDbmsConnection().setTypeMap(map);
     }
 
     @Override
     public void setHoldability(int holdability) throws SQLException {
-    	getDbmsConnection().setHoldability(holdability);
+        getDbmsConnection().setHoldability(holdability);
     }
 
     @Override
@@ -250,22 +250,22 @@ public class VerdictConnection implements Connection {
 
     @Override
     public Savepoint setSavepoint() throws SQLException {
-    	return getDbmsConnection().setSavepoint();
+        return getDbmsConnection().setSavepoint();
     }
 
     @Override
     public Savepoint setSavepoint(String name) throws SQLException {
-    	return getDbmsConnection().setSavepoint(name);
+        return getDbmsConnection().setSavepoint(name);
     }
 
     @Override
     public void rollback(Savepoint savepoint) throws SQLException {
-    	getDbmsConnection().rollback();
+        getDbmsConnection().rollback();
     }
 
     @Override
     public void releaseSavepoint(Savepoint savepoint) throws SQLException {
-    	getDbmsConnection().releaseSavepoint(savepoint);
+        getDbmsConnection().releaseSavepoint(savepoint);
     }
 
     @Override
@@ -275,47 +275,47 @@ public class VerdictConnection implements Connection {
 
     @Override
     public PreparedStatement prepareStatement(String sql, int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support prepared statement");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support prepared statement");
     }
 
     @Override
     public CallableStatement prepareCall(String sql, int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support callable statement");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support callable statement");
     }
 
     @Override
     public PreparedStatement prepareStatement(String sql, int autoGeneratedKeys) throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support prepared statement");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support prepared statement");
     }
 
     @Override
     public PreparedStatement prepareStatement(String sql, int[] columnIndexes) throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support prepared statement");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support prepared statement");
     }
 
     @Override
     public PreparedStatement prepareStatement(String sql, String[] columnNames) throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support prepared statement");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support prepared statement");
     }
 
     @Override
     public Clob createClob() throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support clob type");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support clob type");
     }
 
     @Override
     public Blob createBlob() throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support blob type");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support blob type");
     }
 
     @Override
     public NClob createNClob() throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support nclob type");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support nclob type");
     }
 
     @Override
     public SQLXML createSQLXML() throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support xml type");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support xml type");
     }
 
     @Override
@@ -325,32 +325,32 @@ public class VerdictConnection implements Connection {
 
     @Override
     public void setClientInfo(String name, String value) throws SQLClientInfoException {
-    	getDbmsConnection().setClientInfo(name, value);
+        getDbmsConnection().setClientInfo(name, value);
     }
 
     @Override
     public void setClientInfo(Properties properties) throws SQLClientInfoException {
-    	getDbmsConnection().setClientInfo(properties);
+        getDbmsConnection().setClientInfo(properties);
     }
 
     @Override
     public String getClientInfo(String name) throws SQLException {
-    	return getDbmsConnection().getClientInfo(name);
+        return getDbmsConnection().getClientInfo(name);
     }
 
     @Override
     public Properties getClientInfo() throws SQLException {
-    	return getDbmsConnection().getClientInfo();
+        return getDbmsConnection().getClientInfo();
     }
 
     @Override
     public Array createArrayOf(String typeName, Object[] elements) throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support Array type");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support Array type");
     }
 
     @Override
     public Struct createStruct(String typeName, Object[] attributes) throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support Struct type");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support Struct type");
     }
 
     @Override
@@ -365,26 +365,26 @@ public class VerdictConnection implements Connection {
 
     @Override
     public void abort(Executor executor) throws SQLException {
-    	getDbmsConnection().abort(executor);
+        getDbmsConnection().abort(executor);
     }
 
     @Override
     public void setNetworkTimeout(Executor executor, int milliseconds) throws SQLException {
-    	getDbmsConnection().setNetworkTimeout(executor, milliseconds);
+        getDbmsConnection().setNetworkTimeout(executor, milliseconds);
     }
 
     @Override
     public int getNetworkTimeout() throws SQLException {
-    	return getDbmsConnection().getNetworkTimeout();
+        return getDbmsConnection().getNetworkTimeout();
     }
 
     @Override
     public <T> T unwrap(Class<T> iface) throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support wrap");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support wrap");
     }
 
     @Override
     public boolean isWrapperFor(Class<?> iface) throws SQLException {
-    	throw new SQLFeatureNotSupportedException("Verdict doesn't support wrap");
+        throw new SQLFeatureNotSupportedException("Verdict doesn't support wrap");
     }
 }
