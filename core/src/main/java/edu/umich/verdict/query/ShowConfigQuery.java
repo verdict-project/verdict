a1775dd58ba9b9a5f3a2071f4ec45a31b6d23c2a
Yongjoo Park <pyongjoo@umich.edu>
2017-08-22 23:41:23 -0400

    dummy query added for show config query

--- core/src/main/java/edu/umich/verdict/query/ShowConfigQuery.java
+++ core/src/main/java/edu/umich/verdict/query/ShowConfigQuery.java
@@ -19,6 +19,7 @@ public class ShowConfigQuery extends Query {
 			String value = kv.getValue();
 			System.out.println(key + "\t" + value);
 		}
+		vc.getDbms().execute("select 1");	// dummy
 	}
 
 }
