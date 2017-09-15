a57c12149d77025e182c252ab0bc4a8d37a19916
Yongjoo Park <pyongjoo@umich.edu>
2017-09-15 15:04:19 -0400

    minor

--- core/src/main/java/edu/umich/verdict/VerdictSparkHiveContext.java
+++ core/src/main/java/edu/umich/verdict/VerdictSparkHiveContext.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package edu.umich.verdict;
 
 import java.sql.ResultSet;
@@ -17,37 +34,47 @@ import edu.umich.verdict.util.VerdictLogger;
  *
  */
 public class VerdictSparkHiveContext extends VerdictContext {
-	
-	private DataFrame df;
 
-	public VerdictSparkHiveContext(SparkContext sc) throws VerdictException {
-		this(sc, new VerdictConf());
-	}
-	
-	public VerdictSparkHiveContext(SparkContext sc, VerdictConf conf) throws VerdictException {
-		super(conf);
-		conf.setDbms("spark");
-		HiveContext sqlContext = new HiveContext(sc);
-		setDbms(new DbmsSpark(this, sqlContext));
-		setMeta(new VerdictMeta(this));
-	}
+    private DataFrame df;
+    
+    private HiveContext sqlContext;
 
-	@Override
-	public void execute(String sql) throws VerdictException {
-		VerdictLogger.debug(this, "An input query:");
-		VerdictLogger.debugPretty(this, sql, "  ");
-		Query vq = Query.getInstance(this, sql);
-		df = vq.computeDataFrame();
-	}
+    public VerdictSparkHiveContext(SparkContext sc) throws VerdictException {
+        this(sc, new VerdictConf());
+    }
 
-	@Override
-	public ResultSet getResultSet() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public VerdictSparkHiveContext(SparkContext sc, VerdictConf conf) throws VerdictException {
+        super(conf);
+        conf.setDbms("spark");
+        sqlContext = new HiveContext(sc);
+        setDbms(new DbmsSpark(this, sqlContext));
+        setMeta(new VerdictMeta(this));
+    }
+    
+    /**
+     * Returns the underlying regular HiveContext
+     * @return
+     */
+    public HiveContext getHiveContext() {
+        return sqlContext;
+    }
 
-	@Override
-	public DataFrame getDataFrame() {
-		return df;
-	}
+    @Override
+    public void execute(String sql) throws VerdictException {
+        VerdictLogger.debug(this, "An input query:");
+        VerdictLogger.debugPretty(this, sql, "  ");
+        Query vq = Query.getInstance(this, sql);
+        df = vq.computeDataFrame();
+    }
+
+    @Override
+    public ResultSet getResultSet() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public DataFrame getDataFrame() {
+        return df;
+    }
 }
