3394344202e51a39bb8863434f8dad7cf0cac4fc
Yongjoo Park <pyongjoo@umich.edu>
2017-09-22 01:53:18 -0400

    Yue's spark2 support merged

--- core/src/main/java/edu/umich/verdict/VerdictSpark2HiveContext.java
+++ core/src/main/java/edu/umich/verdict/VerdictSpark2HiveContext.java
@@ -7,7 +7,6 @@ import org.apache.spark.SparkContext;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.hive.HiveContext;
 
 import edu.umich.verdict.dbms.DbmsSpark2;
 import edu.umich.verdict.exceptions.VerdictException;
@@ -15,42 +14,42 @@ import edu.umich.verdict.query.Query;
 import edu.umich.verdict.util.VerdictLogger;
 
 public class VerdictSpark2HiveContext extends VerdictContext {
-	
-	private Dataset<Row> df;
 
-	public VerdictSpark2HiveContext(SparkContext sc) throws VerdictException {
-		this(sc, new VerdictConf());
-	}
-	
-	public VerdictSpark2HiveContext(SparkContext sc, VerdictConf conf) throws VerdictException {
-		super(conf);
-		conf.setDbms("spark2");
-		SparkSession sparkSession = SparkSession.builder().getOrCreate();
-		setDbms(new DbmsSpark2(this, sparkSession));
-		setMeta(new VerdictMeta(this));
-	}
+    private Dataset<Row> df;
 
-	@Override
-	public void execute(String sql) throws VerdictException {
-		VerdictLogger.debug(this, "An input query:");
-		VerdictLogger.debugPretty(this, sql, "  ");
-		Query vq = Query.getInstance(this, sql);
-		df = vq.computeDataset();
-	}
+    public VerdictSpark2HiveContext(SparkContext sc) throws VerdictException {
+        this(sc, new VerdictConf());
+    }
 
-	@Override
-	public ResultSet getResultSet() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public VerdictSpark2HiveContext(SparkContext sc, VerdictConf conf) throws VerdictException {
+        super(conf);
+        conf.setDbms("spark2");
+        SparkSession sparkSession = SparkSession.builder().getOrCreate();
+        setDbms(new DbmsSpark2(this, sparkSession));
+        setMeta(new VerdictMeta(this));
+    }
 
-	@Override
-	public Dataset<Row> getDataset() {
-		return df;
-	}
+    @Override
+    public void execute(String sql) throws VerdictException {
+        VerdictLogger.debug(this, "An input query:");
+        VerdictLogger.debugPretty(this, sql, "  ");
+        Query vq = Query.getInstance(this, sql);
+        df = vq.computeDataset();
+    }
 
-	@Override
-	public DataFrame getDataFrame() {
-		return null;
-	}
+    @Override
+    public ResultSet getResultSet() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Dataset<Row> getDataset() {
+        return df;
+    }
+
+    @Override
+    public DataFrame getDataFrame() {
+        return null;
+    }
 }
