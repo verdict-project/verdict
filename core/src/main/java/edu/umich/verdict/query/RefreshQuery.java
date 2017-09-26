b098b9cf848f51a5fb17aacd9151f30b54edf7fb
Yongjoo Park <pyongjoo@umich.edu>
2017-09-25 23:55:28 -0400

    refresh deletes all cached info

--- core/src/main/java/edu/umich/verdict/query/RefreshQuery.java
+++ core/src/main/java/edu/umich/verdict/query/RefreshQuery.java
@@ -9,34 +9,36 @@ import edu.umich.verdict.util.VerdictLogger;
 
 public class RefreshQuery extends Query {
 
-	public RefreshQuery(VerdictContext vc, String q) {
-		super(vc, q);
-	}
+    public RefreshQuery(VerdictContext vc, String q) {
+        super(vc, q);
+    }
 
-	@Override
-	public void compute() throws VerdictException {
-		VerdictSQLParser p = StringManipulations.parserOf(queryString);
-		VerdictSQLBaseVisitor<String> visitor = new VerdictSQLBaseVisitor<String>() {
-			@Override
-			public String visitRefresh_statement(VerdictSQLParser.Refresh_statementContext ctx) {
-				String schema = null;
-				if (ctx.schema != null) {
-					schema = ctx.schema.getText();
-				}
-				return schema;
-			}
-		};
-		String schema = visitor.visit(p.refresh_statement());
-		schema = (schema != null)? schema : ( (vc.getCurrentSchema().isPresent())? vc.getCurrentSchema().get() : null );
+    @Override
+    public void compute() throws VerdictException {
+        VerdictSQLParser p = StringManipulations.parserOf(queryString);
+        VerdictSQLBaseVisitor<String> visitor = new VerdictSQLBaseVisitor<String>() {
+            @Override
+            public String visitRefresh_statement(VerdictSQLParser.Refresh_statementContext ctx) {
+                String schema = null;
+                if (ctx.schema != null) {
+                    schema = ctx.schema.getText();
+                }
+                return schema;
+            }
+        };
+        String schema = visitor.visit(p.refresh_statement());
+        schema = (schema != null)? schema : ( (vc.getCurrentSchema().isPresent())? vc.getCurrentSchema().get() : null );
 
-		if (schema != null) {
-			vc.getMeta().refreshSampleInfo(schema);
-			vc.getMeta().refreshTables(schema);
-		} else {
-			String msg = "No schema selected. No refresh done.";
-			VerdictLogger.error(msg);
-			throw new VerdictException(msg);
-		}
-	}
+        vc.getMeta().clearSampleInfo();
+        
+        if (schema != null) {
+            vc.getMeta().refreshSampleInfo(schema);
+//            vc.getMeta().refreshTables(schema);
+        } else {
+            String msg = "No schema selected. No refresh done.";
+            VerdictLogger.error(msg);
+            throw new VerdictException(msg);
+        }
+    }
 
 }
