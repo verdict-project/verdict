b59209f626d278687ea0a801295b41064c7fecbb
Dong Young Yoon <dongyoungy@gmail.com>
2018-05-10 20:58:38 -0400

    postgres driver can be used with either 'postgres' or 'postgresql'

--- core/src/main/java/edu/umich/verdict/dbms/Dbms.java
+++ core/src/main/java/edu/umich/verdict/dbms/Dbms.java
@@ -124,7 +124,7 @@ public abstract class Dbms {
             dbms = new DbmsRedshift(vc, dbName, host, port, schema, user, password, jdbcClassName);
         } else if (dbName.equals("dummy")) {
             dbms = new DbmsDummy(vc);
-        } else if (dbName.equals("postgresql")){
+        } else if (dbName.equals("postgresql") || dbName.equals("postgres")){
             dbms = new DbmsPostgreSQL(vc, dbName, host, port, schema, user, password, jdbcClassName);
         } else if (dbName.equals("h2")) {
             dbms = new DbmsH2(vc, dbName, host, port, schema, user, password, jdbcClassName);
