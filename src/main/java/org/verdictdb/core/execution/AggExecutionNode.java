3c513950053c465874aebf936c6e588a1ffaacf1
Yongjoo Park <pyongjoo@umich.edu>
2018-06-01 10:50:11 -0400

    working on async result handler
    Former-commit-id: c6c21e700cfaab6e3eb6e0703ca94de839864619

--- src/main/java/org/verdictdb/core/execution/AggExecutionNode.java
+++ src/main/java/org/verdictdb/core/execution/AggExecutionNode.java
@@ -3,35 +3,69 @@ package org.verdictdb.core.execution;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.tuple.Pair;
+import org.verdictdb.connection.DbmsConnection;
+import org.verdictdb.connection.DbmsQueryResult;
+import org.verdictdb.core.aggresult.AggregateFrame;
+import org.verdictdb.core.query.AbstractRelation;
 import org.verdictdb.core.query.SelectQueryOp;
+import org.verdictdb.core.rewriter.ScrambleMeta;
+import org.verdictdb.core.rewriter.aggresult.AggResultCombiner;
+import org.verdictdb.core.rewriter.aggresult.SingleAggResultRewriter;
+import org.verdictdb.core.rewriter.query.AggQueryRewriter;
+import org.verdictdb.exception.VerdictDbException;
+import org.verdictdb.result.AsyncHandler;
 
 /**
- * Will be used to handle nested queries (i.e., the queries including aggregate queries as subqueries).
+ * Represents an execution of a single aggregate query (without nested components).
  * 
  * @author Yongjoo Park
  *
  */
 public class AggExecutionNode {
   
-  List<AggColumn> aggColumns = new ArrayList<>();
+  DbmsConnection conn;
+  
+  ScrambleMeta meta;
+  
+  List<String> nonaggColumns = new ArrayList<>();
+  
+  /**
+   * Pairs of aggregate column name and its aggregation type
+   */
+  List<Pair<String, String>> aggColumns = new ArrayList<>();
   
   SelectQueryOp originalQuery;
   
-  SelectQueryOp rewrittenQuery;
-
-}
-
-class AggColumn {
+//  SelectQueryOp rewrittenQuery;
   
-  String aliasName;
+  public AggExecutionNode(DbmsConnection conn, ScrambleMeta meta, SelectQueryOp query) {
+    this.conn = conn;
+    this.meta = meta;
+    this.originalQuery = query;
+  }
   
-  String aggType;   // one of "sum", "count", or "avg"
+  public void asyncExecute(AsyncHandler handler) throws VerdictDbException {
+    AggQueryRewriter rewriter = new AggQueryRewriter(meta);
+    List<AbstractRelation> rewrittenQueries = rewriter.rewrite(originalQuery);
+    AggregateFrame combinedAggResult = null;
+    for (int i = 0; i < rewrittenQueries.size(); i++) {
+      AggregateFrame newAggResult = null;
+      if (i == 0) {
+        combinedAggResult = newAggResult;
+      }
+      else {
+        combinedAggResult = AggResultCombiner.combine(combinedAggResult, newAggResult);
+      }
+      SingleAggResultRewriter aggRewriter = new SingleAggResultRewriter(combinedAggResult);
+    }
+    
+    
+  }
   
-  public static AggColumn of(String aliasName, String aggType) {
-    AggColumn a = new AggColumn();
-    a.aliasName = aliasName;
-    a.aggType = aggType;
-    return a;
+  public DbmsQueryResult execute() {
+    return null;
   }
   
 }
+
---
+++ src/main/java/org/verdictdb/core/rewriter/aggresult/AggResultCombiner.java
@@ -0,0 +1,11 @@
+package org.verdictdb.core.rewriter.aggresult;
+
+import org.verdictdb.core.aggresult.AggregateFrame;
+
+public class AggResultCombiner {
+  
+  public static AggregateFrame combine(AggregateFrame agg1, AggregateFrame agg2) {
+    return null;
+  }
+
+}
