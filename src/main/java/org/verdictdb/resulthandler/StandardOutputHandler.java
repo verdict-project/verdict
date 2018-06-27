86bdfd39ba866398458e9fba711c31eba645d4d5
Yongjoo Park <pyongjoo@umich.edu>
2018-06-27 17:40:51 -0400

    skeleton code added
    Former-commit-id: 949a1f07b877f7cb4354adc32fbb4e90150338dc

---
+++ src/main/java/org/verdictdb/resulthandler/StandardOutputHandler.java
@@ -0,0 +1,13 @@
+package org.verdictdb.resulthandler;
+
+import org.verdictdb.connection.DbmsQueryResult;
+
+public class StandardOutputHandler extends AsyncHandler {
+
+  @Override
+  public void handle(DbmsQueryResult result) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
