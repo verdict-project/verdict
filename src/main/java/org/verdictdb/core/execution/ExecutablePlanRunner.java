e3675f3c5dce104b5d0087236b56e6a7adbbb15c
Yongjoo Park <pyongjoo@umich.edu>
2018-07-01 14:47:20 -0400

    before removing run method in QueryExecutionNode
    Former-commit-id: 597f73aec56203de7dff064567b87df61f62f5ae

--- src/main/java/org/verdictdb/core/execution/ExecutablePlanRunner.java
+++ src/main/java/org/verdictdb/core/execution/ExecutablePlanRunner.java
@@ -1,16 +1,11 @@
 package org.verdictdb.core.execution;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import org.verdictdb.core.connection.DbmsConnection;
-import org.verdictdb.core.connection.DbmsQueryResult;
-import org.verdictdb.core.sqlobject.SqlConvertable;
-import org.verdictdb.exception.VerdictDBException;
-import org.verdictdb.sqlreader.QueryToSql;
 
 public class ExecutablePlanRunner {
 
@@ -56,111 +51,6 @@ public class ExecutablePlanRunner {
     }
 
     return reader;
-  }
-
-}
-
-
-class ExecutableNodeRunner implements Runnable {
-
-  DbmsConnection conn;
-
-  ExecutableNode node;
-
-  public ExecutableNodeRunner(DbmsConnection conn, ExecutableNode node) {
-    this.conn = conn;
-    this.node = node;
-  }
-
-  List<ExecutionInfoToken> retrieve() {
-    List<ExecutionTokenQueue> sourceQueues = node.getSourceQueues();
-
-    for (int i = 0; i < sourceQueues.size(); i++) {
-      ExecutionInfoToken rs = sourceQueues.get(i).peek();
-      if (rs == null) {
-        return null;
-      }
-    }
-
-    // all results available now
-    List<ExecutionInfoToken> results = new ArrayList<>();
-    for (int i = 0; i < sourceQueues.size(); i++) {
-      ExecutionInfoToken rs = sourceQueues.get(i).take();
-      results.add(rs);
-    }
-    return results;
-  }
-
-  void broadcast(ExecutionInfoToken token) {
-    for (ExecutionTokenQueue dest : node.getDestinationQueues()) {
-      dest.add(token);
-    }
-  }
-
-  void execute(List<ExecutionInfoToken> tokens) throws VerdictDBException {
-    SqlConvertable sqlObj = node.createQuery(tokens);
-    String sql = QueryToSql.convert(conn.getSyntax(), sqlObj);
-    DbmsQueryResult intermediate = conn.executeQuery(sql);
-    ExecutionInfoToken resultToken = node.createToken(intermediate);
-    broadcast(resultToken);
-  }
-
-  boolean doesIncludeFailure(List<ExecutionInfoToken> tokens) {
-    for (ExecutionInfoToken t : tokens) {
-      if (t.isFailureToken()) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  boolean areAllSuccess(List<ExecutionInfoToken> latestResults) {
-    for (ExecutionInfoToken t : latestResults) {
-      if (t.isStatusToken()) {
-        // do nothing
-      } else {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  @Override
-  public void run() {
-    // no dependency exists
-    if (node.getSourceQueues().size() == 0) {
-      try {
-        execute(Arrays.<ExecutionInfoToken>asList());
-        broadcast(ExecutionInfoToken.successToken());
-        return;
-      } catch (VerdictDBException e) {
-        broadcast(ExecutionInfoToken.failureToken());
-      }
-    }
-
-    // dependency exists
-    while (true) {
-      List<ExecutionInfoToken> tokens = retrieve();
-      if (tokens == null) {
-        continue;
-      }
-      if (doesIncludeFailure(tokens)) {
-        broadcast(ExecutionInfoToken.failureToken());
-        break;
-      }
-      if (areAllSuccess(tokens)) {
-        broadcast(ExecutionInfoToken.successToken());
-        break;
-      }
-      
-      // actual processing
-      try {
-        execute(tokens);
-      } catch (VerdictDBException e) {
-        broadcast(ExecutionInfoToken.failureToken());
-        break;
-      }
-    }
   }
 
 }
