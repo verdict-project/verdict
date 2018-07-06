04919fac88600346cc24ca069e7848afbb28bd85
shucheng <joezhong@umich.edu>
2018-07-06 14:17:19 -0400

    Add isNullable() and getClassName()
    Former-commit-id: a17c21835c75f24b23df4f2f2375e939d54f6f44

--- src/main/java/org/verdictdb/core/connection/SparkDataTypeConverter.java
+++ src/main/java/org/verdictdb/core/connection/SparkDataTypeConverter.java
@@ -10,46 +10,59 @@ import static java.sql.Types.*;
 
 public class SparkDataTypeConverter {
 
-    public static int typeInt(String type) {
-        if (type.equals("BinaryType")) {
-            return BIT;
-        }
-        else if (type.equals("BooleanType")) {
-            return BOOLEAN;
-        }
-        else if (type.equals("DateType")) {
-            return DATE;
-        }
-        else if (type.equals("StringType")) {
-            return VARCHAR;
-        }
-        else if (type.equals("TimestampType")) {
-            return TIMESTAMP;
-        }
-        else if (type.equals("DecimalType")) {
-            return DECIMAL;
-        }
-        else if (type.equals("DoubleType")) {
-            return DOUBLE;
-        }
-        else if (type.equals("FloatType")) {
-            return FLOAT;
-        }
-        else if (type.equals("ByteType")) {
-            return SMALLINT;
-        }
-        else if (type.equals("IntegerType")) {
-            return INTEGER;
-        }
-        else if (type.equals("LongType")) {
-            return INTEGER;
-        }
-        else if (type.equals("ShortType")) {
-            return SMALLINT;
-        }
-        else if (type.equals("ArrayType")) {
-            return ARRAY;
-        }
-        else return OTHER;
+  public static int typeInt(String type) {
+    if (type.equals("BinaryType")) {
+      return BIT;
+    } else if (type.equals("BooleanType")) {
+      return BOOLEAN;
+    } else if (type.equals("DateType")) {
+      return DATE;
+    } else if (type.equals("StringType")) {
+      return VARCHAR;
+    } else if (type.equals("TimestampType")) {
+      return TIMESTAMP;
+    } else if (type.equals("DecimalType")) {
+      return DECIMAL;
+    } else if (type.equals("DoubleType")) {
+      return DOUBLE;
+    } else if (type.equals("FloatType")) {
+      return FLOAT;
+    } else if (type.equals("ByteType")) {
+      return SMALLINT;
+    } else if (type.equals("IntegerType")) {
+      return INTEGER;
+    } else if (type.equals("LongType")) {
+      return BIGINT;
+    } else if (type.equals("ShortType")) {
+      return SMALLINT;
+    } else if (type.equals("ArrayType")) {
+      return ARRAY;
+    } else return OTHER;
+  }
+
+  public static String typeClassName(int type) {
+    if (type == VARCHAR) {
+      return "java.lang.String";
+    } else if (type == BIT || type == BOOLEAN) {
+      return "java.lang.Boolean";
+    } else if (type == DATE) {
+      return "java.sql.Date";
+    } else if (type == TIMESTAMP) {
+      return "java.sql.Timestamp";
+    } else if (type == DECIMAL) {
+      return "java.math.BigDecimal";
+    } else if (type == DOUBLE) {
+      return "java.lang.Double";
+    } else if (type == FLOAT) {
+      return "java.lang.Float";
+    } else if (type == SMALLINT) {
+      return "java.lang.Short";
+    } else if (type == INTEGER) {
+      return "java.lang.Integer";
+    } else if (type == BIGINT) {
+      return "java.lang.Long";
+    } else {
+      return "java.lang.Object";
     }
+  }
 }
--- src/test/java/org/verdictdb/core/connection/SparkConnectionTest.java
+++ src/test/java/org/verdictdb/core/connection/SparkConnectionTest.java
@@ -17,61 +17,61 @@ import static org.junit.Assert.assertEquals;
 
 public class SparkConnectionTest {
 
-    @Test
-    public void testSparkConnectionExecute() throws VerdictDBDbmsException {
-        SparkSession spark = SparkSession.builder().appName("test")
-                .master("local")
-                .config("spark.sql.catalogImplementation", "hive")
-                .getOrCreate();
-        SparkConnection sparkConnection = new SparkConnection(spark, new SparkSyntax());
-        sparkConnection.execute("CREATE SCHEMA IF NOT EXISTS myschema");
-        List<List<Object>> contents = new ArrayList<>();
-        contents.add(Arrays.<Object>asList(1, "Anju"));
-        contents.add(Arrays.<Object>asList(2, "Sonia"));
-        contents.add(Arrays.<Object>asList(3, "Asha"));
+  @Test
+  public void testSparkConnectionExecute() throws VerdictDBDbmsException {
+    SparkSession spark = SparkSession.builder().appName("test")
+        .master("local")
+        .config("spark.sql.catalogImplementation", "hive")
+        .getOrCreate();
+    SparkConnection sparkConnection = new SparkConnection(spark, new SparkSyntax());
+    sparkConnection.execute("CREATE SCHEMA IF NOT EXISTS myschema");
+    List<List<Object>> contents = new ArrayList<>();
+    contents.add(Arrays.<Object>asList(1, "Anju"));
+    contents.add(Arrays.<Object>asList(2, "Sonia"));
+    contents.add(Arrays.<Object>asList(3, "Asha"));
 
-        sparkConnection.execute("CREATE TABLE IF NOT EXISTS myschema.PERSON(id int, name varchar(255))");
-        for (List<Object> row : contents) {
-            String id = row.get(0).toString();
-            String name = row.get(1).toString();
-            sparkConnection.execute(String.format("INSERT INTO myschema.PERSON VALUES(%s, '%s')", id, name));
-        }
-
-        sparkConnection.execute("create table myschema.newtable using parquet partitioned by (id) as select * from myschema.person as t");
-        List<String> partitions = sparkConnection.getPartitionColumns("myschema", "newtable");
-        assertEquals("id=1", partitions.get(0));
-        assertEquals("id=2", partitions.get(1));
-        assertEquals("id=3", partitions.get(2));
-        sparkConnection.execute("DROP TABLE IF EXISTS myschema.PERSON");
-        sparkConnection.execute("DROP TABLE IF EXISTS myschema.newtable");
-        sparkConnection.execute("DROP SCHEMA IF EXISTS myschema");
-
+    sparkConnection.execute("CREATE TABLE IF NOT EXISTS myschema.PERSON(id int, name varchar(255))");
+    for (List<Object> row : contents) {
+      String id = row.get(0).toString();
+      String name = row.get(1).toString();
+      sparkConnection.execute(String.format("INSERT INTO myschema.PERSON VALUES(%s, '%s')", id, name));
     }
 
-    @Test
-    public void testSparkConnection() throws VerdictDBDbmsException {
-        SparkSession spark = SparkSession.builder().appName("test")
-                .master("local")
-                .config("spark.sql.catalogImplementation", "hive")
-                .getOrCreate();
-        spark.read().format("jdbc");
-        SparkConnection sparkConnection = new SparkConnection(spark, new SparkSyntax());
-        sparkConnection.execute("CREATE SCHEMA IF NOT EXISTS myschema");
-        List<List<Object>> contents = new ArrayList<>();
-        contents.add(Arrays.<Object>asList(1, "Anju"));
-        contents.add(Arrays.<Object>asList(2, "Sonia"));
-        contents.add(Arrays.<Object>asList(3, "Asha"));
+    sparkConnection.execute("create table myschema.newtable using parquet partitioned by (id) as select * from myschema.person as t");
+    List<String> partitions = sparkConnection.getPartitionColumns("myschema", "newtable");
+    assertEquals("id=1", partitions.get(0));
+    assertEquals("id=2", partitions.get(1));
+    assertEquals("id=3", partitions.get(2));
+    sparkConnection.execute("DROP TABLE IF EXISTS myschema.PERSON");
+    sparkConnection.execute("DROP TABLE IF EXISTS myschema.newtable");
+    sparkConnection.execute("DROP SCHEMA IF EXISTS myschema");
 
-        sparkConnection.execute("CREATE TABLE IF NOT EXISTS myschema.PERSON(id int, name varchar(255))");
-        List<String> schemas = sparkConnection.getSchemas();
-        assertEquals("default", schemas.get(0));
-        assertEquals("myschema", schemas.get(1));
-        List<String> tables = sparkConnection.getTables("myschema");
-        assertEquals("person", tables.get(0));
-        List<Pair<String, String>> columns = sparkConnection.getColumns("myschema", "person");
-        assertEquals(new ImmutablePair<>("id", "int"), columns.get(0));
-        assertEquals(new ImmutablePair<>("name", "string"), columns.get(1));
-        sparkConnection.execute("DROP TABLE IF EXISTS myschema.PERSON");
-        sparkConnection.execute("DROP SCHEMA IF EXISTS myschema");
-    }
+  }
+
+  @Test
+  public void testSparkConnection() throws VerdictDBDbmsException {
+    SparkSession spark = SparkSession.builder().appName("test")
+        .master("local")
+        .config("spark.sql.catalogImplementation", "hive")
+        .getOrCreate();
+    spark.read().format("jdbc");
+    SparkConnection sparkConnection = new SparkConnection(spark, new SparkSyntax());
+    sparkConnection.execute("CREATE SCHEMA IF NOT EXISTS myschema");
+    List<List<Object>> contents = new ArrayList<>();
+    contents.add(Arrays.<Object>asList(1, "Anju"));
+    contents.add(Arrays.<Object>asList(2, "Sonia"));
+    contents.add(Arrays.<Object>asList(3, "Asha"));
+
+    sparkConnection.execute("CREATE TABLE IF NOT EXISTS myschema.PERSON(id int, name varchar(255))");
+    List<String> schemas = sparkConnection.getSchemas();
+    assertEquals("default", schemas.get(0));
+    assertEquals("myschema", schemas.get(1));
+    List<String> tables = sparkConnection.getTables("myschema");
+    assertEquals("person", tables.get(0));
+    List<Pair<String, String>> columns = sparkConnection.getColumns("myschema", "person");
+    assertEquals(new ImmutablePair<>("id", "int"), columns.get(0));
+    assertEquals(new ImmutablePair<>("name", "string"), columns.get(1));
+    sparkConnection.execute("DROP TABLE IF EXISTS myschema.PERSON");
+    sparkConnection.execute("DROP SCHEMA IF EXISTS myschema");
+  }
 }
