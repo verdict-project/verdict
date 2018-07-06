a6973ca279c43515c5c086187ed2f6e2ab45a196
91017 <zsc910171726@gmail.com>
2018-07-06 01:06:31 -0400

    minor
    Former-commit-id: c9ac3e89c334556666f447b824fb72b4a1a78741

--- src/main/java/org/verdictdb/core/connection/SparkDataTypeConverter.java
+++ src/main/java/org/verdictdb/core/connection/SparkDataTypeConverter.java
@@ -10,46 +10,33 @@ import static java.sql.Types.*;
 
 public class SparkDataTypeConverter {
 
-    public static int typeInt(String type) {
-        if (type.equals("BinaryType")) {
-            return BIT;
-        }
-        else if (type.equals("BooleanType")) {
-            return BOOLEAN;
-        }
-        else if (type.equals("DateType")) {
-            return DATE;
-        }
-        else if (type.equals("StringType")) {
-            return VARCHAR;
-        }
-        else if (type.equals("TimestampType")) {
-            return TIMESTAMP;
-        }
-        else if (type.equals("DecimalType")) {
-            return DECIMAL;
-        }
-        else if (type.equals("DoubleType")) {
-            return DOUBLE;
-        }
-        else if (type.equals("FloatType")) {
-            return FLOAT;
-        }
-        else if (type.equals("ByteType")) {
-            return SMALLINT;
-        }
-        else if (type.equals("IntegerType")) {
-            return INTEGER;
-        }
-        else if (type.equals("LongType")) {
-            return INTEGER;
-        }
-        else if (type.equals("ShortType")) {
-            return SMALLINT;
-        }
-        else if (type.equals("ArrayType")) {
-            return ARRAY;
-        }
-        else return OTHER;
-    }
+  public static int typeInt(String type) {
+    if (type.equals("BinaryType")) {
+      return BIT;
+    } else if (type.equals("BooleanType")) {
+      return BOOLEAN;
+    } else if (type.equals("DateType")) {
+      return DATE;
+    } else if (type.equals("StringType")) {
+      return VARCHAR;
+    } else if (type.equals("TimestampType")) {
+      return TIMESTAMP;
+    } else if (type.equals("DecimalType")) {
+      return DECIMAL;
+    } else if (type.equals("DoubleType")) {
+      return DOUBLE;
+    } else if (type.equals("FloatType")) {
+      return FLOAT;
+    } else if (type.equals("ByteType")) {
+      return SMALLINT;
+    } else if (type.equals("IntegerType")) {
+      return INTEGER;
+    } else if (type.equals("LongType")) {
+      return INTEGER;
+    } else if (type.equals("ShortType")) {
+      return SMALLINT;
+    } else if (type.equals("ArrayType")) {
+      return ARRAY;
+    } else return OTHER;
+  }
 }
