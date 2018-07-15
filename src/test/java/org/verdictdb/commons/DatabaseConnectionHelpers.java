56ee0a7c098580e6e50cd0c5a4608b600075de46
91017 <zsc910171726@gmail.com>
2018-07-15 17:06:32 -0400

    setup spark

--- src/test/java/org/verdictdb/commons/DatabaseConnectionHelpers.java
+++ src/test/java/org/verdictdb/commons/DatabaseConnectionHelpers.java
@@ -29,6 +29,127 @@ public class DatabaseConnectionHelpers {
     spark.sql(String.format("CREATE SCHEMA IF NOT EXISTS `%s`", schema));
     
     // create tables
+    spark.sql(String.format(
+        "CREATE TABLE IF NOT EXISTS `%s`.`nation` (" +
+        "  `n_nationkey`  INT, " +
+        "  `n_name`       CHAR(25), " +
+        "  `n_regionkey`  INT, " +
+        "  `n_comment`    VARCHAR(152), " +
+        "  `n_dummy`      VARCHAR(10)) " +
+        "ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' " +
+        "STORED AS TEXTFILE ",
+          schema));
+    spark.sql(String.format("LOAD DATA local inpath 'src/test/resources/tpch_old_test_data/nation.tbl' overwrite into table %s.nation", schema));
+    spark.sql(String.format(
+        "CREATE TABLE IF NOT EXISTS `%s`.`region` (" +
+        "  `r_regionkey`  INT, " +
+        "  `r_name`       CHAR(25), " +
+        "  `r_comment`    VARCHAR(152), " +
+        "  `r_dummy`      VARCHAR(10)) " +
+        "ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' " +
+        "STORED AS TEXTFILE ",
+          schema));
+    spark.sql(String.format("LOAD DATA local inpath 'src/test/resources/tpch_old_test_data/region.tbl' overwrite into table %s.region", schema));
+    spark.sql(String.format(
+        "CREATE TABLE IF NOT EXISTS `%s`.`part`  (" +
+        "  `p_partkey`     INT, " +
+        "  `p_name`        VARCHAR(55), " +
+        "  `p_mfgr`        CHAR(25), " +
+        "  `p_brand`       CHAR(10), " +
+        "  `p_type`        VARCHAR(25), " +
+        "  `p_size`        INT, " +
+        "  `p_container`   CHAR(10), " +
+        "  `p_retailprice` DECIMAL(15,2) , " +
+        "  `p_comment`     VARCHAR(23) , " +
+        "  `p_dummy`       VARCHAR(10)) " +
+        "ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' " +
+        "STORED AS textfile ",
+          schema));
+    spark.sql(String.format("LOAD DATA local inpath 'src/test/resources/tpch_old_test_data/part.tbl' overwrite into table %s.part", schema));
+    spark.sql(String.format(
+        "CREATE TABLE IF NOT EXISTS `%s`.`supplier` ( " +
+        "  `s_suppkey`     INT , " +
+        "  `s_name`        CHAR(25) , " +
+        "  `s_address`     VARCHAR(40) , " +
+        "  `s_nationkey`   INT , " +
+        "  `s_phone`       CHAR(15) , " +
+        "  `s_acctbal`     DECIMAL(15,2) , " +
+        "  `s_comment`     VARCHAR(101), " +
+        "  `s_dummy`       VARCHAR(10)) " +
+        "ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' " +
+        "STORED AS textfile ",
+          schema));
+    spark.sql(String.format("LOAD DATA local inpath 'src/test/resources/tpch_old_test_data/supplier.tbl' overwrite into table %s.supplier", schema));
+    spark.sql(String.format(
+        "CREATE TABLE IF NOT EXISTS `%s`.`partsupp` ( " +
+        "  `ps_partkey`     INT , " +
+        "  `ps_suppkey`     INT , " +
+        "  `ps_availqty`    INT , " +
+        "  `ps_supplycost`  DECIMAL(15,2)  , " +
+        "  `ps_comment`     VARCHAR(199), " +
+        "  `ps_dummy`       VARCHAR(10)) " +
+        "ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' " +
+        "STORED AS textfile ",
+          schema));
+    spark.sql(String.format("LOAD DATA local inpath 'src/test/resources/tpch_old_test_data/partsupp.tbl' overwrite into table %s.partsupp", schema));
+    spark.sql(String.format(
+        "CREATE TABLE IF NOT EXISTS `%s`.`customer` (" +
+        "  `c_custkey`     INT , " +
+        "  `c_name`        VARCHAR(25) , " +
+        "  `c_address`     VARCHAR(40) , " +
+        "  `c_nationkey`   INT , " +
+        "  `c_phone`       CHAR(15) , " +
+        "  `c_acctbal`     DECIMAL(15,2)   , " +
+        "  `c_mktsegment`  CHAR(10) , " +
+        "  `c_comment`     VARCHAR(117), " +
+        "  `c_dummy`       VARCHAR(10)) " +
+        "ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' " +
+        "STORED AS textfile ",
+          schema));
+    spark.sql(String.format("LOAD DATA local inpath 'src/test/resources/tpch_old_test_data/customer.tbl' overwrite into table %s.customer", schema));
+    spark.sql(String.format(
+        "CREATE TABLE IF NOT EXISTS  `%s`.`orders`  ( " +
+        "  `o_orderkey`       INT , " +
+        "  `o_custkey`        INT , " +
+        "  `o_orderstatus`    CHAR(1) , " +
+        "  `o_totalprice`     DECIMAL(15,2) , " +
+        "  `o_orderdate`      DATE , " +
+        "  `o_orderpriority`  CHAR(15) , " +
+        "  `o_clerk`          CHAR(15) , " +
+        "  `o_shippriority`   INT , " +
+        "  `o_comment`        VARCHAR(79), " +
+        "  `o_dummy`          VARCHAR(10)) " +
+        "ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' " +
+        "STORED AS textfile ",
+          schema));
+    spark.sql(String.format("LOAD DATA local inpath 'src/test/resources/tpch_old_test_data/orders.tbl' overwrite into table %s.orders", schema));
+    spark.sql(String.format(
+        "CREATE TABLE IF NOT EXISTS `%s`.`lineitem` (" +
+        "  `l_orderkey`       INT , " +
+        "  `l_partkey`        INT , " +
+        "  `l_suppkey`        INT , " +
+        "  `l_linenumber`     INT , " +
+        "  `l_quantity`       DECIMAL(15,2) , " +
+        "  `l_extendedprice`  DECIMAL(15,2) , " +
+        "  `l_discount`       DECIMAL(15,2) , " +
+        "  `l_tax`            DECIMAL(15,2) , " +
+        "  `l_returnflag`     CHAR(1) , " +
+        "  `l_linestatus`     CHAR(1) , " +
+        "  `l_shipdate`       DATE , " +
+        "  `l_commitdate`     DATE , " +
+        "  `l_receiptdate`    DATE , " +
+        "  `l_shipinstruct`   CHAR(25) , " +
+        "  `l_shipmode`       CHAR(10) , " +
+        "  `l_comment`        VARCHAR(44), " +
+        "  `l_dummy`          VARCHAR(10))" +
+        "ROW FORMAT DELIMITED FIELDS TERMINATED BY '|' " +
+        "STORED AS textfile ",
+          schema));
+    spark.sql(String.format("LOAD DATA local inpath 'src/test/resources/tpch_old_test_data/lineitem.tbl' overwrite into table %s.lineitem", schema));
+
+
+    /*
+    // create tables
     String datafilePath = new File("src/test/resources/tpch_test_data/").getAbsolutePath();
     spark.sql(String.format(
         "CREATE EXTERNAL TABLE IF NOT EXISTS `%s`.`nation` (" +
@@ -125,7 +246,7 @@ public class DatabaseConnectionHelpers {
         "LOCATION '%s/orders'",
           schema, datafilePath));
     spark.sql(String.format(
-        "CREATE EXTERNAL TABLE IF NOT EXISTS `%s`.`lineitem` (" + 
+        "CREATE EXTERNAL TABLE IF NOT EXISTS `%s`.`lineitem` (" +
         "  `l_orderkey`       INT , " +
         "  `l_partkey`        INT , " +
         "  `l_suppkey`        INT , " +
@@ -147,7 +268,7 @@ public class DatabaseConnectionHelpers {
         "STORED AS textfile " +
         "LOCATION '%s/lineitem'",
           schema, datafilePath));
-    
+     */
     return spark;
   }
 
