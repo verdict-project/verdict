5ef209b68f3a1e28c479005f75cd04fff6b2621c
Yongjoo Park <pyongjoo@umich.edu>
2018-07-15 16:46:48 -0400

    sortkey is used instead for redshift partitioning

--- src/main/java/org/verdictdb/sqlsyntax/RedshiftSyntax.java
+++ src/main/java/org/verdictdb/sqlsyntax/RedshiftSyntax.java
@@ -66,11 +66,39 @@ public class RedshiftSyntax extends SqlSyntax {
         "on table_schema='" + schema + "' and table_name = '" + table + "'";
   }
 
+  /**
+   * Documentation about SORTKEY: https://docs.aws.amazon.com/redshift/latest/dg/r_CREATE_TABLE_NEW.html
+   * 
+   * If you do not specify any sort keys, the table is not sorted by default. You can
+   * define a maximum of 400 COMPOUND SORTKEY columns or 8 INTERLEAVED SORTKEY
+   * columns per table.
+   * 
+   * COMPOUND
+   * Specifies that the data is sorted using a compound key made up of all of the
+   * listed columns, in the order they are listed. A compound sort key is most useful
+   * when a query scans rows according to the order of the sort columns. The
+   * performance benefits of sorting with a compound key decrease when queries rely
+   * on secondary sort columns. You can define a maximum of 400 COMPOUND SORTKEY
+   * columns per table.
+   * 
+   * INTERLEAVED
+   * Specifies that the data is sorted using an interleaved sort key. A maximum of
+   * eight columns can be specified for an interleaved sort key.
+   * 
+   * An interleaved sort gives equal weight to each column, or subset of columns, in
+   * the sort key, so queries do not depend on the order of the columns in the sort
+   * key. When a query uses one or more secondary sort columns, interleaved sorting
+   * significantly improves query performance. Interleaved sorting carries a small
+   * overhead cost for data loading and vacuuming operations.
+   * 
+   * Important
+   * 
+   * Don’t use an interleaved sort key on columns with monotonically increasing
+   * attributes, such as identity columns, dates, or timestamps.
+   */
   @Override
   public String getPartitionByInCreateTable() {
-    // not implemented yet
-    // redshift does not support partitioning; thus, we must use SORTKEY
-    return null;
+    return "COMPOUND SORTKEY";
   }
 
   @Override
