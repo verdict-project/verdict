80b2bbedb33e17c62fe281971dcae68148cd3921
Yongjoo Park <pyongjoo@umich.edu>
2018-07-18 14:36:20 -0400

    group by number tests added

--- src/main/java/org/verdictdb/sqlreader/RelationGen.java
+++ src/main/java/org/verdictdb/sqlreader/RelationGen.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.verdictdb.core.sqlobject.AbstractRelation;
-import org.verdictdb.core.sqlobject.AliasReference;
 import org.verdictdb.core.sqlobject.AliasedColumn;
 import org.verdictdb.core.sqlobject.AsteriskColumn;
 import org.verdictdb.core.sqlobject.BaseColumn;
