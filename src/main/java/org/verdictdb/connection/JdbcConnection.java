089f908946c068fd26ccae0b1be6555d39643e5f
Yongjoo Park <pyongjoo@umich.edu>
2018-07-22 21:11:31 -0400

    circleCI file added back

--- src/main/java/org/verdictdb/connection/JdbcConnection.java
+++ src/main/java/org/verdictdb/connection/JdbcConnection.java
@@ -6,29 +6,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.text.StrMatcher;
-import org.apache.commons.lang3.text.StrTokenizer;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
-import org.stringtemplate.v4.ST;
 import org.verdictdb.exception.VerdictDBDbmsException;
 import org.verdictdb.sqlsyntax.HiveSyntax;
+import org.verdictdb.sqlsyntax.ImpalaSyntax;
 import org.verdictdb.sqlsyntax.PostgresqlSyntax;
+import org.verdictdb.sqlsyntax.RedshiftSyntax;
 import org.verdictdb.sqlsyntax.SparkSyntax;
 import org.verdictdb.sqlsyntax.SqlSyntax;
 import org.verdictdb.sqlsyntax.SqlSyntaxList;
 
 import com.google.common.collect.Sets;
-
-import org.verdictdb.sqlsyntax.*;
 
 public class JdbcConnection implements DbmsConnection {
 
