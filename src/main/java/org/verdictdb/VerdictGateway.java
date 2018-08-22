6207265934c05673c162d7fd43b343240b89526b
Tim Shen <xyshen@umich.edu>
2018-08-22 12:09:39 +0800

    finish basic java connector functionality

--- src/main/java/org/verdictdb/VerdictGateway.java
+++ src/main/java/org/verdictdb/VerdictGateway.java
@@ -1,21 +1,21 @@
 package org.verdictdb;
 
+import org.verdictdb.coordinator.VerdictSingleResult;
 import org.verdictdb.exception.VerdictDBException;
 import py4j.GatewayServer;
 
 public class VerdictGateway {
     public static void main(String[] args) {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            VerdictContext v = VerdictContext.fromConnectionString("jdbc:mysql://localhost:3306/instacart", "root", "");
-            GatewayServer gatewayServer = new GatewayServer(v);
+//            VerdictContext v = VerdictContext.fromConnectionString("jdbc:mysql://localhost:3306/instacart", "root", "");
+//            VerdictSingleResult r = v.sql("select count(1) from instacart.orders_joined");
+//            r.next();
+//            System.out.println(r.getInt(0));
+            GatewayServer gatewayServer = new GatewayServer(new VerdictContext());
             gatewayServer.start();
             System.out.println("Gateway Server Started");
         }
         catch (VerdictDBException e){
-            e.printStackTrace();
-        }
-        catch (ClassNotFoundException e){
             e.printStackTrace();
         }
 
