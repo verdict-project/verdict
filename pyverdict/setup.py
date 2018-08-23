2bcd139db076591d1068e0ee7a017584f78ddf61
Tim <xyshen@umich.edu>
2018-08-22 23:40:31 -0400

    minor

---
+++ pyverdict/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+import os
+
+setup(name='pyverdict',
+      version='0.0.1',
+      description='Python connector for VerdictDB',
+      url='',
+      author='',
+      author_email='',
+      license='',
+      packages=setuptools.find_packages(),
+      package_data={'pyverdict': ['lib/*.jar']},
+      include_package_data=True
+ )
+
