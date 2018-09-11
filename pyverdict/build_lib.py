e3ea39f4a8c55271c534d7881e5ceb9d38558a0a
Yongjoo Park <pyongjoo@umich.edu>
2018-09-10 20:50:32 -0400

    another trial

--- pyverdict/build_lib.py
+++ pyverdict/build_lib.py
@@ -31,7 +31,7 @@ def build_and_copy(root_dir, lib_dir):
         ['mvn', '-DskipTests', '-DtestPhase=false', '-DpackagePhase=true', 'clean', 'package'])
     subprocess.check_call(['rm', '-rf', os.path.join(lib_dir, '*verdictdb*.jar')])
     subprocess.check_call(['cp', os.path.join('target', jar_name), lib_dir])
-
+    os.chdir(root_dir)
 
 if __name__ == "__main__":
     build_and_copy()
