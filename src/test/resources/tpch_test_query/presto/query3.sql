a6305f911744e6619d6593859276c04f22b7f32b
Dong Young Yoon <dongyoungy@gmail.com>
2018-11-02 11:14:06 -0400

    fixed a bug in ScrambleMetaStore where it does not properly gathers scramble information + added a directory for presto tpch queries (only a few works for presto for now)

---
+++ src/test/resources/tpch_test_query/presto/query3.sql
@@ -0,0 +1,21 @@
+select
+    l_orderkey,
+    sum(l_extendedprice * (1 - l_discount)) as revenue,
+    o_orderdate, o_shippriority
+from
+    customer,
+    orders,
+    lineitem
+where
+    c_custkey = o_custkey
+    and l_orderkey = o_orderkey
+    and date(o_orderdate) < date '1998-12-01'
+    and date(l_shipdate) > date '1996-12-01'
+group by
+    l_orderkey,
+    o_orderdate,
+    o_shippriority
+order by
+    revenue desc,
+    o_orderdate
+limit 10
