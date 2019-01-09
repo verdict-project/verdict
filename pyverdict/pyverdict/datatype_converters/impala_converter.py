cc00cc1c2539eb7dbeed2656e1929c8c53c4dd98
Jarrid Rector-Brooks <jrectorb@umich.edu>
2019-01-09 13:23:05 -0500

    Add type conversion rule comment

--- pyverdict/pyverdict/datatype_converters/impala_converter.py
+++ pyverdict/pyverdict/datatype_converters/impala_converter.py
@@ -9,6 +9,24 @@ _typename_to_converter_fxn = {'timestamp': _str_to_datetime}
 
 
 class ImpalaConverter(DatatypeConverterBase):
+    '''
+    Type conversion rule:
+
+        BIGINT           => int,
+        BOOLEAN          => bool,
+        CHAR             => str,
+        DECIMAL          => decimal.Decimal,
+        DOUBLE           => float,
+        FLOAT            => float,
+        REAL             => float,
+        SMALLINT         => int,
+        STRING           => str,
+        TIMESTAMP        => datetime.datetime,
+        TINYINT          => int,
+        VARCHAR          => str
+
+    '''
+
     @staticmethod
     def read_value(result_set, index, col_typename):
         if col_typename in _typename_to_converter_fxn:
