d643f5cfa1d9a027e171a3aa29f6c1b1b4ffc663
Yongjoo Park <pyongjoo@umich.edu>
2019-01-14 13:46:19 -0500

    added timeout in retrieving a token

--- src/main/java/org/verdictdb/core/execplan/ExecutionTokenQueue.java
+++ src/main/java/org/verdictdb/core/execplan/ExecutionTokenQueue.java
@@ -19,6 +19,7 @@ package org.verdictdb.core.execplan;
 import java.io.Serializable;
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
@@ -39,7 +40,12 @@ public class ExecutionTokenQueue implements Serializable {
 
   public ExecutionInfoToken take() {
     try {
-      return internalQueue.takeFirst();
+//      return internalQueue.takeFirst();
+      ExecutionInfoToken t = internalQueue.poll(1, TimeUnit.MINUTES);
+      if (t == null) {
+        throw new RuntimeException("The internal token was unavailable for a long time for an unknown reasons.");
+      }
+      return t;
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
