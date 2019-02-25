b045e7936db09f5b7bf609494aa5b22330bbd188
91017 <joezhong@umich.edu>
2019-02-24 21:01:45 -0500

    synchronize take() method

--- src/main/java/org/verdictdb/core/execplan/ExecutionTokenQueue.java
+++ src/main/java/org/verdictdb/core/execplan/ExecutionTokenQueue.java
@@ -38,10 +38,10 @@ public class ExecutionTokenQueue implements Serializable {
     return internalQueue.poll();
   }
 
-  public ExecutionInfoToken take() {
+  public synchronized ExecutionInfoToken take() {
     try {
 //      return internalQueue.takeFirst();
-      ExecutionInfoToken t = internalQueue.poll(5, TimeUnit.MINUTES);
+      ExecutionInfoToken t = internalQueue.poll(1, TimeUnit.MINUTES);
       if (t == null) {
         throw new RuntimeException("The internal token was unavailable for a long time for an unknown reasons.");
       }
